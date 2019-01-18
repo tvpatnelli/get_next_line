59091dbd4660d1e42aaf3f4113febbeaa1617fbb
Victoria PATNELLI <vpatnell@e2r6p7.42.fr>
2019-01-18 17:21:50 +0100

    ebauch3

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: vpatnell <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/08 13:52:49 by vpatnell          #+#    #+#             */
-/*   Updated: 2019/01/15 15:35:02 by vpatnell         ###   ########.fr       */
+/*   Updated: 2019/01/18 17:20:37 by vpatnell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,50 +14,61 @@
 #include "libft.h"
 #include <stdio.h>
 
+static char *chr_n(const int fd,char  *buff)
+{
+	char *tmp;
+	char *str;
+
+	tmp = ft_strchr(buff, '\n');
+	str = ft_strnew(BUFF_SIZE);
+
+	if (tmp)
+	{
+		*tmp = '\0';
+		str = ft_strjoin(str , buff);
+		*buff = *tmp + 1;
+	}
+	else
+		str = ft_strjoin(str , buff);
+	return (str);
+}
 
 
 int		get_next_line(const int fd, char **line)
 {
-	char buff[BUFF_SIZE + 1];
-	char *tmp;
-	char *str;
+	static char buff[BUFF_SIZE + 1];
 	int ret;
-	static char	 *rest;
 
+	/*ft_putstr("////////// value  ////////\n");
+	ft_putstr(buff);
+	ft_putstr("\n////////// ////////\n");*/
 	*line = ft_strnew(0);
-	tmp = ft_strchr(buff , '\n');
-	if (fd)
+	if  (fd)
 	{
 		while ((ret = read(fd, buff , BUFF_SIZE)) > 0)
-		{
-			printf("\nbuff -> %s\n", buff);
-			if(tmp)
+			if (ret)
 			{
-				*tmp ='\0';
-				*line = ft_strjoin(*line, buff);
-				*buff = *tmp + 1;
-				printf("line -> %s", *line);
-				printf("buff??? -> %s\n", buff);
-				return (0);
+				*line = ft_strjoin(*line, chr_n(fd, buff));
+				return (1);
 			}
-			else
-				*line = ft_strjoin(*line, buff);
-				printf("%s", *line);
-		}
-		return (1);
+		else
+			return (0);
 	}
 	return (0);
 }
+
 
 int		main(int arc, char **arv)
 {
 	int		fd;
 	char	*line;
+	int count = 0;
 
 	fd = open(arv[1], O_RDONLY);
-	while (get_next_line(fd, &line) == 1)
+	while (get_next_line(fd, &line) > 0)
 	{
-		ft_putstr(line);
+		count++;
+		ft_putendl(line);
 			free(line);
 	}
 	close(fd);
